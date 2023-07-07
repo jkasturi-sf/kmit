cf98bf1223777917f994188ea8605ebe439e9021
Jaipal Reddy Kasturi <jkasturi@salesforce.com>
2023-07-07 11:52:58 +0530

    Update math_functions_test.py

--- math_functions_test.py
+++ math_functions_test.py
@@ -3,4 +3,4 @@ from math_functions import Math
 
 class MathFunctionsTest(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(Math.add(2,3), 5)
+        self.assertEqual(Math.add(2,3), 6)
